be909faf074ebb6f192a573c7afe71ce907d2807
NIRAJ KUMAR <nirajvnec@gmail.com>
2020-09-26 12:47:25 +0530

    Added User Entity

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -16,5 +16,11 @@ namespace DatingApp.API.Data
         // }
 
         public DbSet<Value> Values {get; set;}
+        public DbSet<User> Users {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasKey( u => u.Id);
+        }
     }
 }
